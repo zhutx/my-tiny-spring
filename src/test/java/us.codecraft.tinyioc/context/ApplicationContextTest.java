d55c22b15a77de884ea00b507330c04b711f8a9f
zhutx <passwd_1986>
2018-04-04 13:10:29 +0800

    commit

--- src/test/java/us.codecraft.tinyioc/context/ApplicationContextTest.java
+++ src/test/java/us.codecraft.tinyioc/context/ApplicationContextTest.java
@@ -7,6 +7,7 @@ public class ApplicationContextTest {
 
     @Test
     public void test() throws Exception {
+
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("tinyioc.xml");
         HelloWorldService helloWorldService = (HelloWorldService) applicationContext.getBean("helloWorldService");
         helloWorldService.helloWorld();
